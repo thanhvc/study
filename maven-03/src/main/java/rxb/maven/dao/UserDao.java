963546fcc16e8c3011b90e17f015e6021412383b
bo729892905 <wonderfulxiaobo@gmail.com>
2015-01-11 12:35:20 +0800

    add comment

--- maven-03/src/main/java/rxb/maven/dao/UserDao.java
+++ maven-03/src/main/java/rxb/maven/dao/UserDao.java
@@ -3,6 +3,17 @@ package rxb.maven.dao;
 import rxb.maven.model.User;
 
 public interface UserDao {
+	/**
+	 * 创建用户
+	 * @param user
+	 * @return 是否创建成功
+	 */
 	public boolean createUser(User user);
+	
+	/**
+	 * 删除用户
+	 * @param id
+	 * @return 是否删除成功
+	 */
 	public boolean deleteUserById(Integer id);
 }
